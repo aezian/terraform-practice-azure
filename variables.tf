4ffc424f015420a17eded3fae8a8d244166ec419
Muhammad Ehsan Mansoor <ehsan.786@hotmail.com>
2026-08-17 06:26:26 +0200

    Add Azure storange account with Terraform

--- variables.tf
+++ variables.tf
@@ -12,3 +12,8 @@ variable "tags" {
 description ="common tags for Azure resources"
 type =map(string)
 }
+
+variable "storage_account_name" {
+  description = "Name of the Azure Storage Account"
+  type        = string
+}
