ff3b4527c89fae7c7cb4cc4cfbdeff105dcc15c1
Muhammad Ehsan Mansoor <ehsan.786@hotmail.com>
2026-09-02 02:43:40 +0200

    Manage AKS ACR pull permission with Terraform

--- main.tf
+++ main.tf
@@ -29,3 +29,28 @@ resource "azurerm_container_registry" "main" {
 
   tags = var.tags
 }
+
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = var.aks_cluster_name
+  location = var.aks_location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = var.aks_dns_prefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.aks_node_count
+    vm_size    = var.aks_vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                = azurerm_container_registry.main.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
+}
